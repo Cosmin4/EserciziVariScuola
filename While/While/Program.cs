ecf46a8e6aff22507ebd3c9d0dfc975e17831b49
cosmin dirlea <cdirlea4@gmail.com>
2015-10-01 12:36:37 +0200

    Aggiornato il programma con while, ora va tutto.

--- While/While/Program.cs
+++ While/While/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace While
 {
@@ -10,6 +11,29 @@ namespace While
     {
         static void Main(string[] args)
         {
+            string numero = "";
+            int n = 0;
+            int somma = 0;
+
+            var myReader = new StreamReader("Valori.txt");
+
+            while (numero != null)
+            {
+                numero = myReader.ReadLine();
+                try
+                {
+                    n = Convert.ToInt32(numero);
+                    somma += n;
+                }
+                catch (Exception) { }
+
+
+                Console.WriteLine(numero);
+            }
+
+            myReader.Close();
+            Console.WriteLine("Somma : " + somma);
+            Console.ReadKey();
         }
     }
 }
